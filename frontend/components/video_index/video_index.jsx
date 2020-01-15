caab07a4b06f05a86951e79354f20642d5d9bc64
derek schear <dschear94@gmail.com>
2020-01-15 09:35:56 -0500

    Prep for heroku push

--- frontend/components/video_index/video_index.jsx
+++ frontend/components/video_index/video_index.jsx
@@ -1,5 +1,44 @@
 import React from 'react';
+import { gapi } from 'gapi-script';
+// import 
 
-const VideoIndex = () => <h1>PAQ Video Index</h1>;
+class VideoIndex extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        // this.gapiAuth();
+    }
+
+    gapiAuth() {
+        // gapi
+    }
+
+    fetchVideos() {
+
+        // const requestOptions = {
+        //     playlistId: "UUvO6uJUVJQ6SrATfsWR5_aA",
+        //     part: 'snippet',
+        // };
+
+        // const request = gapi.client.youtube.playlistItems.list(requestOptions);
+
+        // request.execute(response => {
+        //     console.log(response);
+        // });
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>PAQ Video Index</h1>
+                <button onClick={this.gapiAuth()}>Authenticate</button>
+                <button onClick={this.fetchVideos()}>Refresh List</button>
+            </div>
+        );
+    }
+}
 
 export default VideoIndex;
